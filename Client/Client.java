0486e794426245ccfa7faefa632cf8d715f5abec
AlexanderPapachristu <55894236+AlexanderPapachristu@users.noreply.github.com>
2021-11-10 16:06:07 -0500

    Add files via upload

---
+++ Client/Client.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+class Client {
+	
+	//driver code
+	public static void main(String[]args)
+	{
+		try (Socket socket = new Socket("localhost", 1234))
+		{
+			//writing to server
+			PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+			
+			//reading from server
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			Scanner sc = new Scanner(System.in);
+			String line = "";
+			while(!line.equalsIgnoreCase("exit")){
+			System.out.println("Please enter an option: Math, Time, Palindrome, Binary, Hex\nOr enter exit to exit the code.");
+			String option = sc.nextLine();
+			
+			//****function to hold all the work to solve math problems*********
+			if(option.equalsIgnoreCase("math")){
+				System.out.println("please input the name of the file containing basic math operations");
+				String fileName = sc.nextLine();
+				File myFile = new File (fileName);
+				long startTime = System.currentTimeMillis();
+				if(myFile.exists() && !myFile.isDirectory()){
+					//reading from file
+					Scanner fl = new Scanner(myFile);
+					
+					//while (!"exit".equalsIgnoreCase(line)){
+					while(fl.hasNextLine()){
+						//read line from user
+						
+						line = fl.nextLine();
+						
+						// sending the user input to server
+						out.println(line);
+						out.flush();
+						
+						//displaying server reply
+						System.out.println("User Input: " + line);
+						System.out.println("Answer: " + in.readLine());
+					}
+				}
+				else{
+					System.err.println("Error: File not found");
+				}
+			long endTime = System.currentTimeMillis();
+			//close scanner object
+			sc.close();
+			System.out.println("Code executed in: " + (endTime-startTime) + " ms");
+			}
+			
+			//********Function to deal with returning a time*************
+			if(option.equalsIgnoreCase("Time")){
+				out.println("Time"); //send to server to activate function
+				out.flush();
+				System.out.println("Current Date and Time: " + in.readLine()); //return the date and time
+			}
+			
+			//*****Check Palindrome**********
+			if(option.equalsIgnoreCase("Palindrome")){
+				System.out.println("What would you like to check for a palendrome?");
+				String pal = sc.nextLine();
+				option += pal;
+				out.println(option); //send to server to activate function
+				out.flush();
+				System.out.println(in.readLine()); //return if the text is a palindrome
+			}
+			
+			//********Convert decimal to binary
+			if(option.equalsIgnoreCase("Binary")){
+				System.out.println("What number would you like to convert to binary?");
+				String num = sc.nextLine();
+				option += num;
+				out.println(option);
+				out.flush();
+				System.out.println(in.readLine());
+			}
+			
+			//********Convert decimal to Hex
+			if(option.equalsIgnoreCase("Hex")){
+				System.out.println("What number would you like to convert to Hexidecimal?");
+				String num = sc.nextLine();
+				option += num;
+				out.println(option);
+				out.flush();
+				System.out.println(in.readLine());
+			}
+			}
+		}
+		catch (IOException e ){
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Server/Server.java
@@ -0,0 +1,191 @@
+import java.io.*;
+import java.net.*;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;   
+
+class Server {
+	public static void main(String[]args)
+	{
+		ServerSocket server = null;
+		
+		try{
+			//server listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+			
+			//run infinite loop for getting client request
+			while(true){
+				
+				//socket to accept incoming client requests
+				Socket client = server.accept();
+				
+				//Display a new client is connected
+				System.out.println("New Client Connected" + client.getInetAddress().getHostAddress());
+				
+				//create new thread object
+				ClientHandler clientSocket = new ClientHandler(client);
+				
+				//This thread will handle the client seperately
+				new Thread(clientSocket).start();
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		finally {
+			if(server != null){
+				try{
+					server.close();
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
+
+class ClientHandler implements Runnable {
+	private final Socket clientSocket;
+	
+	//Constructor
+	public ClientHandler(Socket socket)
+	{
+		this.clientSocket = socket;
+	}
+	
+	public void run()
+	{
+		PrintWriter out = null;
+		BufferedReader in = null;
+		
+		try {
+			
+			//get the outstream of the client
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			
+			//get input stream from the client
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			
+			String line;
+			while ((line = in.readLine()) != null) {
+
+				//System.out.printf("Sent from the client: %s\n", line); //writing the recieved message from client
+				
+				if(line.contains("+"))
+					out.println(math(line));
+				else if(line.contains("-"))
+					out.println(math(line));
+				else if(line.contains("*"))
+					out.println(math(line));
+				else if(line.contains("/"))
+					out.println(math(line));
+				else if(line.equalsIgnoreCase("Time"))
+					out.println(getDateTime());
+				else if(line.contains("Palindrome") || line.contains("palindrome")){
+					String pal = line.replace("(?i)palindrome", "");
+					out.println(checkPalindrome(pal));
+				}
+				else if(line.contains("Binary") || line.contains("binary")){
+					String decimal = line.replaceAll("(?i)binary", "");
+					out.println(decToBin(decimal));
+				}
+				else if(line.contains("Hex") || line.contains("hex")){
+					String decimal = line.replaceAll("(?i)hex", "");
+					out.println(decToHex(decimal));
+				}
+					
+				
+				
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try{
+				if(out != null){
+					out.close();
+				}
+				if(in != null){
+					in.close();
+					clientSocket.close();
+				}
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static String math(String message){
+		String sign = "";
+		
+		if(message.contains("+"))
+			sign = "+";
+		else if(message.contains("-"))
+			sign = "-";
+		else if(message.contains("*"))
+			sign = "*";
+		else if(message.contains("/"))
+			sign = "/";
+		else
+			return ("Input is not a basic math operation");
+		
+		String [] msgParts = message.split("[+*/]|(?<=\\s)-"); //break the string into each part seperated by each operation
+		
+		double num1 = Double.parseDouble(msgParts[0]);
+		double num2 = Double.parseDouble(msgParts[1]);
+		
+		double ans = 0;
+		switch(sign){ //determine the sign from the user input and compute the answer
+			case "+":
+				ans = num1 + num2;
+				break;
+			case "-":
+				ans = num1 - num2;
+				break;
+			case "*":
+				ans = num1 * num2;
+				break;
+			case "/":
+				ans = num1 / num2;
+				break;
+			default:
+				System.out.println("Input is not readable");
+		}
+		String answer = Double.toString(ans); //convert answer to string to send the full answer back to the client
+		return answer;
+	}
+	
+	public static String getDateTime(){
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"); //format the date and time
+		LocalDateTime now = LocalDateTime.now(); //get the current date and time 
+		String answer = dtf.format(now); //save value to string
+		return(answer);  
+	}
+	
+	public static String checkPalindrome(String pal){
+		String revString = "";
+		
+		for(int i = (pal.length() - 1); i >= 0; i--){
+			revString += pal.charAt(i);
+		}
+		if(revString.equalsIgnoreCase(pal))
+			return "The inputted text is a pelindrome";
+		else
+			return "The inputted string is not a palindrome";
+	}
+	
+	public static String decToBin(String decimal){
+		int dec = Integer.parseInt(decimal);
+		String bin = Integer.toBinaryString(dec);
+		return bin;
+	}
+	public static String decToHex(String decimal){
+		int dec = Integer.parseInt(decimal);
+		String hex = Integer.toHexString(dec);
+		return hex;
+	}
+}
